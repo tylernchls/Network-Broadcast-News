4c3f16b09574311ceb6a7467bcf6b3a4c16722ef
Tyler Nichols <tylernchls@gmail.com>
2016-12-28 13:51:27 -1000

    started on adding banned words to kick user off

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const net = require('net');
+const bannedWords = require('./bannedWords.js');
+
 
 var clients = [];
 
+// creates new socket connection
 var server = net.createServer((socket) => {
   console.log('New remote connection detected: ' + socket.remoteAddress);
   clients.push(socket);  // stores each socket that connects in the array
   socket.on('error', (err) => {
     throw err;
   });
-
-
 
   /*
   first: grabs first piece of data, checks if userName is undefined and sets userName.
@@ -18,54 +19,54 @@ var server = net.createServer((socket) => {
 
   */
 
-
   socket.on('data', (data) => {
+      console.log(data.toString());
     if(socket.userName == undefined) {
-    socket.userName = data.toString();
-    console.log(socket.userName.trim() + ' has joined that chat');
+      socket.userName = data.toString().trim();
+      console.log(socket.userName + ' has joined that chat');
 
 
+    } else if(data.toString() === bannedWords){
+          return true;
     } else {
 
       for(var i = 0; i < clients.length; i++) {
         if(clients[i] === socket) {  // won't write data to socket sending the data
 
         } else {
-            clients[i].write(socket.userName.trim() + ' says: ' + data.toString());
+            clients[i].write(socket.userName + ' says: ' + data.toString());
         }
       }
+
     }
 
+
   });
+
 
   // removes socket from array when disconnected
   socket.on('end', () => {
     for(var i = clients.length - 1; i >= 0; i--) {
       if(clients[i] === socket) {
           clients.splice(i, 1);
-          // console.log(clients);
+          console.log(socket.userName + ' has disconnected');
       }
     }
 
   });
 
-
-
-
+  // errors if the server emits the error event
+  server.on('error', (err) => {
+    throw err;
+  });
 
 });
 
-
-
-// errors if the server emits the error event
-server.on('error', (err) => {
-  throw err;
-});
 
 // gives server ability to write admin message to all connected sockets
 process.stdin.on('data', (data) => {
   for(var i = 0; i < clients.length; i++) {
-    clients[i].write(data.toString());
+    clients[i].write('Admin: ' + data.toString());
 
   }
 
@@ -75,6 +76,17 @@ process.stdin.on('data', (data) => {
 server.listen(6969, () => {
   console.log('opened server on', server.address());
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
