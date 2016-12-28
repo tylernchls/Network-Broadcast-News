f6512b34aab90c03a4a6cf2baf6aa5a939910797
Tyler Nichols <tylernchls@gmail.com>
2016-12-27 19:34:50 -1000

    added conditional so data wont be sent to socket sending the data

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const net = require('net');
 var clients = [];
 
 var server = net.createServer((socket) => {
+  console.log('connected: ' + socket.remoteAddress);
   clients.push(socket);  // stores each socket that connects in the array
   socket.on('error', (err) => {
     throw err;
@@ -11,18 +12,31 @@ var server = net.createServer((socket) => {
   // gets incoming data from client and writes it back to all sockets connected/stored in array
   socket.on('data', (data) => {
 
-    for(var i = 0; i <clients.length; i++){
-      clients[i].write(data.toString());
-    }
+    for(var i = 0; i < clients.length; i++) {
+      if(clients[i] === socket) {  // won't write data to socket sending the data
+        return;
+      } else {
+          clients[i].write(data.toString());
+      }
 
+    }
   });
+
 
 
 
   socket.on('end', () => {
-    console.log('someone disconnected;<');
+    for(var i = clients.length - 1; i >= 0; i--) {
+      if(clients[i] === socket) {
+          clients.splice(i, 1);
+          // console.log(clients);
+      }
+
+    }
+
   });
 });
+
 
 
 
