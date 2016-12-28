79a75ac0b43814a5cba29d8e228f32ab14eabfa8
Tyler Nichols <tylernchls@gmail.com>
2016-12-27 19:01:38 -1000

    got server to push data to all connected sockets

--- client.js
+++ client.js
@@ -2,12 +2,18 @@ const net = require('net');
 
 
 
+// allows user to type in data and sent to server
 const client = net.connect({port: 6969}, () => {
   console.log('connected to server');
-  process.stdin.on('data', function(data) {
+  process.stdin.on('data', (data) => {
     client.write(data.toString());
   });
 });
 
+// allows data sent from server to be sent to user and displayed
+client.on('data', (data) => {
+  process.stdout.write(data.toString());
+
+});
 
 
--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const net = require('net');
 
+var clients = [];
+
 var server = net.createServer((socket) => {
-  console.log('connection made');
+  clients.push(socket);  // stores each socket that connects in the array
   socket.on('error', (err) => {
     throw err;
   });
 
+  // gets incoming data from client and writes it back to all sockets connected/stored in array
   socket.on('data', (data) => {
-    console.log('you sent data:', data.toString());
+
+    for(var i = 0; i <clients.length; i++){
+      clients[i].write(data.toString());
+    }
+
   });
+
+
 
   socket.on('end', () => {
     console.log('someone disconnected;<');
   });
 });
+
+
+
 
 // errors if the server emits the error event
 server.on('error', (err) => {
